420cb78e335b895ee6927bfd2967264f48b67c7a
Kyle Qian <kylecqian@gmail.com>
2020-04-26 21:55:53 -0700

    Unity commit

---
+++ Assets/_SCRIPTS/Attraction.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Attraction : MonoBehaviour
+{
+    [SerializeField] Transform attractor_;
+    [SerializeField] float thrust_ = 100;
+
+    Rigidbody rigidBody_;
+
+    void Awake()
+    {
+        rigidBody_ = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        rigidBody_.AddForce(thrust_ * (attractor_.position - transform.position));
+    }
+}
---
+++ Assets/_SCRIPTS/Wow.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Wow : MonoBehaviour
+{
+    [SerializeField] Transform me_;
+    [SerializeField] float leakFactor_ = 0.8f;
+
+    ChuckSubInstance chuck_;
+    float integrator_;
+    Vector3 prevPos_;
+    bool firstPosRecorded_;
+
+    void Awake()
+    {
+        chuck_ = GetComponent<ChuckSubInstance>();
+        chuck_.RunFile("bowing.ck", true);
+    }
+    
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (!firstPosRecorded_)
+        {
+            prevPos_ = pos;
+            firstPosRecorded_ = true;
+            return;
+        }
+
+        float distanceFromMe = Vector3.Distance(pos, me_.position);
+        Vector3 diff = pos - prevPos_;
+
+        // Put magnitude into leaky integrator
+        integrator_ += diff.magnitude;
+
+        // Leak!
+        integrator_ *= leakFactor_;
+
+        prevPos_ = pos;
+
+        chuck_.SetFloat("bowIntensity", integrator_);
+        chuck_.SetFloat("thePitch", 24 + 24 * distanceFromMe);
+    }
+}
